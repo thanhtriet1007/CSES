f1b7fcaf80c0d0565216870751c4f5bc44c702f4
Trie <thanhtriet1007@gmail.com>
2026-05-08 09:19:49 +0700

    SCC

---
+++ Graph/1683.cpp
@@ -0,0 +1,86 @@
+/* Author : Triet Do Thanh - FPT University */
+
+#include <bits/stdc++.h>
+
+#define int            long long
+
+using namespace        std;
+
+typedef pair<int, int> ii;
+
+const int       N      = 1e6 + 7;
+const long long INF     = 1e18 + 7;
+const long long MOD    = 1e9 + 7;
+
+int n, m;
+vector<int>adj[N];
+vector<int>rev[N];
+
+stack<int>myStack;
+int vis[N];
+int scc[N];
+
+int cnt = 0;
+
+void dfs1(int u) {
+    vis[u] = 1;
+    for (int &v : adj[u]) {
+        if (vis[v]) continue;
+        dfs1(v);
+    }
+    myStack.push(u);
+}
+
+void dfs2(int u) {
+    vis[u] = 1;
+    scc[u] = cnt;
+    for (int &v : rev[u]) {
+        if (vis[v]) continue;
+        dfs2(v);
+    }
+}
+
+void solve() {
+    //World Final when?
+    cin >> n >> m;
+    for (int i = 1; i <= m; ++i) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        rev[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        if (!vis[i]) dfs1(i);
+    }
+    for (int i = 1; i <= n; ++i) vis[i] = 0;
+
+    while (!myStack.empty()) {
+        int u = myStack.top();
+        myStack.pop();
+
+        if (vis[u]) continue;
+        ++cnt;
+        dfs2(u);
+    }
+    cout << cnt << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << scc[i] << ' ';
+    }
+
+}
+
+#define TASK "test"
+
+signed main()
+{
+   ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+   if (fopen("input.txt", "r")) {
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+   }
+   int t = 1;
+   //cin >> t;
+   while (t--)
+   solve();
+   return 0;
+}
